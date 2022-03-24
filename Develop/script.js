acaf0ec38ba3a69067044a5b66c409b3d50c27c4
Brady Mavetz <b.mavetz@outlook.com>
2022-03-24 01:08:28 -0500

    Modified script to include prompts for what to put in password, and passowrd length

--- Develop/script.js
+++ Develop/script.js
@@ -5,10 +5,43 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Establish variables with password characters
+var aplhaLow = "abcdefghijklmnopqrstuvwxyz".split("");
+var alphaUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var num = "0123456789".split("");
+var special = "!#$%@^&*><?".split("");
+
+function generatePassword() {
+var numChar =0;
+//Clicking generate password leads to prompts of password requirements
+  for(numChar;8>numChar || 128<numChar;){
+    numChar = window.prompt("Please enter a password length between 8 and 128 characters");
+    // if(numChar>128){
+      // numChar = window.prompt("Please enter a password length between 8 and 128 characters");
+    // }
+  }
+var inclLowercase = false;
+var inclUppercase = false;
+var inclNum = false;
+var inclSpChar
+for(;!inclLowercase && !inclUppercase && !inclNum && !inclSpChar;){
+window.alert("Please select at least one of the following character options for your password: capital letters, lowercase letters, numbers, and/or special characters");
+inclLowercase = window.confirm("Do you wish to include lowercase letters? Press 'OK' to confirm, or 'cancel' to deny");
+inclUppercase = window.confirm("Do you wish to include uppercase letters? Press 'OK' to confirm, or 'cancel' to deny");
+inclNum = window.confirm("Do you wish to include numbers? Press 'OK' to confirm, or 'cancel' to deny");
+inclSpChar = window.confirm("Do you wish to include special characters? Press 'OK' to confirm, or 'cancel' to deny");
+}
+
+//password requirements store answers in variables for passwords
+//generate random password, ensuring 1 of each character type is selected
+//password matches all selected prompts
+//password is displayed to the page
+}
