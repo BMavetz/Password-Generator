3dd5451b0e32c14906018fb644e322569d86776c
Brady Mavetz <b.mavetz@outlook.com>
2022-03-24 01:33:35 -0500

    Included code to establish character field for the password

--- Develop/script.js
+++ Develop/script.js
@@ -11,37 +11,50 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+//Clicking generate password leads to prompts of password requirements
 generateBtn.addEventListener("click", writePassword);
 
 //Establish variables with password characters
-var aplhaLow = "abcdefghijklmnopqrstuvwxyz".split("");
+var alphaLow = "abcdefghijklmnopqrstuvwxyz".split("");
 var alphaUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var num = "0123456789".split("");
 var special = "!#$%@^&*><?".split("");
 
 function generatePassword() {
+  //password requirements store answers in variables for passwords
 var numChar =0;
-//Clicking generate password leads to prompts of password requirements
   for(numChar;8>numChar || 128<numChar;){
     numChar = window.prompt("Please enter a password length between 8 and 128 characters");
-    // if(numChar>128){
-      // numChar = window.prompt("Please enter a password length between 8 and 128 characters");
-    // }
   }
 var inclLowercase = false;
 var inclUppercase = false;
 var inclNum = false;
-var inclSpChar
+var inclSpChar = false;
 for(;!inclLowercase && !inclUppercase && !inclNum && !inclSpChar;){
 window.alert("Please select at least one of the following character options for your password: capital letters, lowercase letters, numbers, and/or special characters");
 inclLowercase = window.confirm("Do you wish to include lowercase letters? Press 'OK' to confirm, or 'cancel' to deny");
 inclUppercase = window.confirm("Do you wish to include uppercase letters? Press 'OK' to confirm, or 'cancel' to deny");
 inclNum = window.confirm("Do you wish to include numbers? Press 'OK' to confirm, or 'cancel' to deny");
 inclSpChar = window.confirm("Do you wish to include special characters? Press 'OK' to confirm, or 'cancel' to deny");
+//generate random password, ensuring 1 of each character type is selected
+var interim = [];
+var passChar;
+if(inclLowercase){
+  passChar = interim.concat(alphaLow);
+  interim = passChar;
+}if(inclUppercase){
+  passChar = interim.concat(alphaUp);
+  interim = passChar;
+}if(inclNum){
+  passChar = interim.concat(num);
+  interim = passChar;
+}if(inclSpChar){
+  passChar = interim.concat(special);
+}
+console.log(passChar);
+return "awesome";
 }
 
-//password requirements store answers in variables for passwords
-//generate random password, ensuring 1 of each character type is selected
 //password matches all selected prompts
 //password is displayed to the page
 }
